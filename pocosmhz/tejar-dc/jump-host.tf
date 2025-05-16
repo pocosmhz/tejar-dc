ea05218e65a2e26cb1f7cb857270aa7e787410e2
Manuel Molina Cuberos <mmc@pocosmhz.org>
2025-05-16 03:14:14 +0200

    Format fixes.

--- pocosmhz/tejar-dc/jump-host.tf
+++ pocosmhz/tejar-dc/jump-host.tf
@@ -6,19 +6,19 @@ module "pm_jump_host" {
   ip_gateway         = var.proxmox_jump_host.ip_gateway
   node_id            = var.proxmox_jump_host.node
   disks_datastore_id = var.proxmox_datastore.disks_datastore.id
-  disk_size          = 3  # 3 GB is the minimum size for cloud images
+  disk_size          = 3 # 3 GB is the minimum size for cloud images
   memory             = 384
   network_bridge     = var.proxmox_network.bridge.id
   image_id           = module.pm_ve_vm_debian_cloud_image[var.proxmox_jump_host.node].id
   timezone           = var.proxmox_timezone
   admin_users        = var.admin_users
-  hosts              = [
+  hosts = [
     {
-      name = "test-debian12"
-      ip   = "192.168.18.150"
+      name    = "test-debian12"
+      ip      = "192.168.18.150"
       ssh_key = tls_private_key.debiantest_pk.private_key_pem
     }
   ]
-  ha_group           = proxmox_virtual_environment_hagroup.pm_ve_hagroups["pve01"].id
-  tags               = ["debian", "ssh-piper"]
+  ha_group = proxmox_virtual_environment_hagroup.pm_ve_hagroups["pve01"].id
+  tags     = ["debian", "ssh-piper"]
 }
