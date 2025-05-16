f5d4f94e127b6f59ce8bf2559dd9d2ac459ace81
Manuel Molina Cuberos <mmc@pocosmhz.org>
2025-05-16 03:06:52 +0200

    pm_ve_vm_sshpiper: New module for creating a jump host / bastion host. It provides access to other internal hosts for the sake of orchestrating through SSH.

---
+++ pocosmhz/tejar-dc/modules/pm_ve_vm_sshpiper/providers.tf
@@ -0,0 +1,13 @@
+terraform {
+  required_version = ">= 1.1, < 1.11.4"
+  required_providers {
+    proxmox = {
+      source  = "bpg/proxmox"
+      version = "0.78.0"
+    }
+    tls = {
+      source  = "hashicorp/tls"
+      version = "4.1.0"
+    }
+  }
+}
