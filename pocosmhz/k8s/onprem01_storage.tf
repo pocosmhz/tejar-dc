9c86e0bd6975274a89669359a76b18062b90d961
Manuel Molina Cuberos <mmc@pocosmhz.org>
2025-07-04 15:29:00 +0200

    pocosmhz/k8s: Added Ceph storage through Helm chart. Provided storage to Kubernetes cluster `onprem01`.

--- pocosmhz/k8s/onprem01_storage.tf
+++ pocosmhz/k8s/onprem01_storage.tf
@@ -9,7 +9,10 @@ resource "helm_release" "ceph_csi_rbd" {
   repository = "https://ceph.github.io/csi-charts"
   chart      = "ceph-csi-rbd"
   version    = "3.14.1"
-
-  namespace = kubernetes_namespace.ceph_csi_rbd.metadata[0].name
-
+  namespace  = kubernetes_namespace.ceph_csi_rbd.metadata[0].name
+  values = [
+    templatefile("${path.module}/source/helm/ceph/ceph-csi-rbd-values.tpl.yml", {
+      ceph_conf = var.k8s_clusters["onprem01"].ceph
+    })
+  ]
 }
---
+++ pocosmhz/k8s/variables.tf
@@ -0,0 +1,41 @@
+variable "k8s_clusters" {
+  description = "Kubernetes clusters configuration"
+  type = map(object({
+    nodes = map(object({
+      ip_address = string
+      ip_gateway = string
+    }))
+    ceph = object({
+      username     = string
+      key          = string
+      mon_hosts    = list(string)
+      cluster_fsid = string
+      rbd_pool     = string
+    })
+  }))
+  default = {
+    k8s01 = {
+      nodes = {
+        k8s01cp01 = {
+          ip_address = "192.168.1.5"
+          ip_gateway = "192.168.1.1"
+        }
+        k8s01cp02 = {
+          ip_address = "192.168.1.6"
+          ip_gateway = "192.168.1.1"
+        }
+      }
+      ceph = {
+        # we omit the client. prefix !
+        username = "kubernetes"
+        key      = "ABCABCABCABCABCABCABC4dQ=="
+        mon_hosts = [
+          "192.168.5.1:6789",
+          "192.168.5.2:6789"
+        ]
+        cluster_fsid = "f4444444-c5a3-4599-af36-b8888b64c0f3"
+        rbd_pool     = "kubernetes"
+      }
+    }
+  }
+}
