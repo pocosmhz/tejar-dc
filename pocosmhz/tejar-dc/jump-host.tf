a7116608d78805683b80e248ce1aaaab1f1c9b25
Manuel Molina Cuberos <mmc@pocosmhz.org>
2026-02-08 16:49:39 +0100

    pocosmhz/tejar-dc: Allow SSH access to standalone Linux hosts through the jump host.

--- pocosmhz/tejar-dc/jump-host.tf
+++ pocosmhz/tejar-dc/jump-host.tf
@@ -1,3 +1,17 @@
+# Let's gather all the hosts we're giving access through this:
+locals {
+  all_accessible_hosts = concat(
+    flatten([for k, v in module.k8s_clusters : v.hosts]),
+    [for k, v in module.pm_linux_hosts : {
+      name           = k
+      ip             = v.ip
+      ssh_key        = v.ssh_key
+      ssh_public_key = v.ssh_public_key
+      id             = v.id
+    }]
+  )
+}
+
 # Bastion host we will need for different tasks
 module "pm_jump_host" {
   source             = "./modules/pm_ve_vm_sshpiper"
@@ -12,9 +26,7 @@ module "pm_jump_host" {
   image_id           = module.pm_ve_vm_debian13_cloud_image[var.proxmox_jump_host.node].id
   timezone           = var.proxmox_timezone
   admin_users        = var.admin_users
-  hosts = flatten(
-    [for k, v in module.k8s_clusters : v.hosts]
-  )
+  hosts              = local.all_accessible_hosts
   # ha_group = proxmox_virtual_environment_hagroup.pm_ve_hagroups["pve01"].id
-  tags     = ["debian", "ssh-piper"]
+  tags = ["debian", "ssh-piper"]
 }
